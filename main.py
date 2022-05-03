4cd6828a477afb2a40a826a5ee3bc66e35c7dfe2
Re-Ki <rekireki788@gmail.com>
2022-05-03 13:50:30 +0630

    testCommit

---
+++ main.py
@@ -0,0 +1,12 @@
+#keyword argument
+#print ( standard output)
+
+a = 1.12345
+print('The value of a is %.3f \n'%a)
+
+print('{name} Hello From {org} \n'.format(name='WintHtut',org='GreenHackers'))
+
+#input
+#input() function or method
+data = input('Please enter somethings :')
+print('user input data is :',data)
